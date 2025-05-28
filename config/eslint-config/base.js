a40406e6e1058e0c7bc79c69444ed2d72f0b1754
Fernanda <fernandakagami@gmail.com>
2025-05-28 14:32:19 -0300

    Add eslint and prettier

--- config/eslint-config/base.js
+++ config/eslint-config/base.js
@@ -1,32 +1,12 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
-
 /**
  * A shared ESLint configuration for the repository.
  *
- * @type {import("eslint").Linter.Config[]}
+ * @type {import('eslint').Linter.Config}
  * */
-export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
+export const config = {
+  extends: ['@rocketseat/eslint-config/react'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
   },
-  {
-    plugins: {
-      onlyWarn,
-    },
-  },
-  {
-    ignores: ["dist/**"],
-  },
-];
+}
--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 
 export const config = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
