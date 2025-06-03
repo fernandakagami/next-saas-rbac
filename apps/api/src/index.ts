fe20dc2a0c2bb443c7a6838120bc58f66b2348c2
Fernanda <fernandakagami@gmail.com>
2025-06-03 09:31:47 -0300

    Add permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,10 @@
 import { defineAbilityFor } from '@saas/auth'
+import { projectSchema } from '@saas/auth/src/models/project'
 
-const ability = defineAbilityFor({ role: "MEMBER" })
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElese = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElese)
-console.log(userCanDeleteOtherUsers)
-console.log(userCannotDeleteOtherUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
